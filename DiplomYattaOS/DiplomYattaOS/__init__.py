ae458679267324f1bf09573835d61bb91cf5d5af
femzek <femzek@DESKTOP-O91F1N8>
2023-11-21 06:24:32 +0300

    Добавьте файлы проекта.

---
+++ DiplomYattaOS/DiplomYattaOS/__init__.py
@@ -0,0 +1,3 @@
+"""
+Package for DiplomYattaOS.
+"""
---
+++ DiplomYattaOS/env/Scripts/django-admin.py
@@ -0,0 +1,5 @@
+#!C:\Users\femzek\source\repos\DiplomYattaOS\DiplomYattaOS\env\Scripts\python.exe
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
